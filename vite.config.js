aa0d66dc9bfe79de33d222fefa0709bd26376a60
Designer-PEG <designer.professionaledgeglobal@gmail.com>
2025-07-28 16:28:55 +0545

    updated for domain deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/everestclaims/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
